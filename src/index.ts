029fe3b19f922023e5af824c18846f64dddea48c
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2023-03-24 13:14:31 +0100

    Export more types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { captureError, config, load, track } from './commands';
+import { CronitorRUMConfig } from './config';
+import { CronitorRUMEvent } from './collect';
 
-export { load, config, track, captureError };
+export { load, config, track, captureError, CronitorRUMConfig, CronitorRUMEvent };
